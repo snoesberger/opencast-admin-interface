4c803ca2891fbee9b2e420a49e9e82dc82109ffd
ensiyehe <ensiyeh.esfandiari@gmail.com>
2025-08-06 09:30:30 +0200

    Resolve the keyboard-nav by pressing tab

--- src/components/shared/modals/Modal.tsx
+++ src/components/shared/modals/Modal.tsx
@@ -1,4 +1,10 @@
-import { forwardRef, PropsWithChildren, useCallback, useImperativeHandle, useState } from "react";
+import {
+  forwardRef,
+  PropsWithChildren,
+  useCallback,
+  useImperativeHandle,
+  useState,
+} from "react";
 import ReactDOM from "react-dom";
 import { useHotkeys } from "react-hotkeys-hook";
 import { availableHotkeys } from "../../../configs/hotkeysConfig";
@@ -7,83 +13,87 @@ import ButtonLikeAnchor from "../ButtonLikeAnchor";
 import { FocusTrap } from "focus-trap-react";
 
 export type ModalProps = {
-	open?: boolean
-	// Having this return false will prevent the modal from closing
-	closeCallback?: () => boolean
-	/** If true, the first element in the modal automatically be focused. If false,
-	 * no element is initially focused */
-	initialFocus?: false | string
-	header: string
-	classId: string
-	className?: string
+  open?: boolean;
+  // Having this return false will prevent the modal from closing
+  closeCallback?: () => boolean;
+  /** If true, the first element in the modal automatically be focused. If false,
+   * no element is initially focused */
+  initialFocus?: false | string;
+  header: string;
+  classId: string;
+  className?: string;
 };
 
 export type ModalHandle = {
-	open: () => void;
-	close?: () => void;
-	isOpen?: () => boolean;
+  open: () => void;
+  close?: () => void;
+  isOpen?: () => boolean;
 };
 
-export const Modal = forwardRef<ModalHandle, PropsWithChildren<ModalProps>>(({
-	open = false,
-	closeCallback,
-	header,
-	classId,
-	className,
-	children,
-}, ref) => {
+export const Modal = forwardRef<ModalHandle, PropsWithChildren<ModalProps>>(
+  (
+    { open = false, closeCallback, header, classId, className, children },
+    ref,
+  ) => {
+    const { t } = useTranslation();
 
-	const { t } = useTranslation();
+    const [isOpen, setOpen] = useState(open);
+    const close = useCallback(() => {
+      if (closeCallback !== undefined && !closeCallback()) {
+        // Don't close modal
+        return;
+      }
+      setOpen(false);
+    }, [closeCallback]);
 
-	const [isOpen, setOpen] = useState(open);
-	const close = useCallback(() => {
-		if (closeCallback !== undefined && !closeCallback()) {
-			// Don't close modal
-			return;
-		}
-		setOpen(false);
-	}, [closeCallback]);
+    useImperativeHandle(
+      ref,
+      () => ({
+        isOpen: () => isOpen,
+        open: () => setOpen(true),
+        close,
+      }),
+      [close, isOpen],
+    );
 
-	useImperativeHandle(ref, () => ({
-		isOpen: () => isOpen,
-		open: () => setOpen(true),
-		close,
-	}), [close, isOpen]);
+    useHotkeys(
+      availableHotkeys.general.CLOSE_MODAL.sequence,
+      close,
+      {
+        description:
+          t(availableHotkeys.general.CLOSE_MODAL.description) ?? undefined,
+      },
+      [ref],
+    );
 
-	useHotkeys(
-		availableHotkeys.general.CLOSE_MODAL.sequence,
-		close,
-		{ description: t(availableHotkeys.general.CLOSE_MODAL.description) ?? undefined },
-		[ref],
-	);
+    return ReactDOM.createPortal(
+      isOpen && (
+        <FocusTrap
+          focusTrapOptions={{
+            escapeDeactivates: false,
+          }} // Prevent escape from deactivating the trap
+        >
+          <div>
+            <div className="modal-animation modal-overlay" />
+            <section
+              id={classId}
+              className={className ? className : "modal wizard modal-animation"}
+            >
+              <header>
+                <ButtonLikeAnchor
+                  extraClassName="fa fa-times close-modal"
+                  onClick={close}
+                  tabIndex={0}
+                />
+                <h2>{header}</h2>
+              </header>
 
-	return ReactDOM.createPortal(
-		isOpen &&
-			<FocusTrap focusTrapOptions={{
-			escapeDeactivates: false }} // Prevent escape from deactivating the trap
-			>
-				<div>
-					<div className="modal-animation modal-overlay" />
-					<section
-						id={classId}
-						className={className ? className : "modal wizard modal-animation"}
-					>
-						<header>
-							<ButtonLikeAnchor
-								extraClassName="fa fa-times close-modal"
-								onClick={close}
-								tabIndex={0}
-							/>
-							<h2>
-								{header}
-							</h2>
-						</header>
-
-							{children}
-					</section>
-				</div>
-			</FocusTrap>,
-		document.body,
-	);
-
-});
+              {children}
+            </section>
+          </div>
+        </FocusTrap>
+      ),
+      document.body,
+    );
+  },
+);
