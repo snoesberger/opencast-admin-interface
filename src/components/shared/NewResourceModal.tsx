1cd62c49236df191b0c4737bc9c97af8e30b2d88
ensiyehe <ensiyeh.esfandiari@gmail.com>
2025-08-18 10:22:33 +0200

    build error

--- src/components/shared/NewResourceModal.tsx
+++ src/components/shared/NewResourceModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useTranslation } from "react-i18next";
 import NewEventWizard from "../events/partials/wizards/NewEventWizard";
 import NewSeriesWizard from "../events/partials/wizards/NewSeriesWizard";
